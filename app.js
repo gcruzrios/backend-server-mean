3ebbe6c0207cd1ea1dc8c31b1c655d7eea898bbe
Greivin Cruz <gcruzr@gmail.com>
2020-05-01 12:19:45 -0600

    Primer commit backend Server

---
+++ app.js
@@ -0,0 +1,29 @@
+// Requires
+var express= require('express');
+var mongoose = require('mongoose');
+
+//Inicializar variables
+var app = express();
+
+
+//Conexión a la base de datos
+
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
+    if (err ) throw err;
+    console.log('Base de datos Puerto 27017: \x1b[32m%s\xb1[0m', 'online');
+})
+//Rutas
+
+app.get('/', (req, res, next ) => {
+    res.status(200).json({
+        ok: true,
+        mensaje:'Petición realizada correctamente'
+    });
+
+});
+
+// Escuchar peticiones
+
+app.listen(3000,()=>{
+    console.log('Express Server Puerto 3000: \x1b[32m%s\xb1[0m', 'online');
+});
